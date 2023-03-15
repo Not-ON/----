164cae73c420f1c04828c1783071391605ac2956
Almrahsi <124255362+Almrahsi@users.noreply.github.com>
2023-03-15 11:57:17 +0800

    Add files via upload

---
+++ Books_Managerment_System/src/main/java/com/yuteng/BMS/user/borrow/servlet/borrowServlet.java
@@ -0,0 +1,29 @@
+package com.yunteng.user.borrow.servlet;
+import com.yunteng.user.borrow.service.service;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/borrowServlet")
+public class borrowServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        service s = new service();
+        String userName = (String) req.getSession().getAttribute("user");
+        try {
+            out.print(s.show(userName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doGet(req, resp);
+    }
+}
